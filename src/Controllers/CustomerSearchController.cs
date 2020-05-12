e83473d944c85fdc7f2d31bc6b9086459c84a4ee
DanteXYZ <47111008+DanteXYZ@users.noreply.github.com>
2020-05-11 19:36:47 -0500

    Some controllers added

---
+++ src/Controllers/CustomerSearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DefCanApi.Controllers
+{
+    public class CustomerSearchController : ApiController//Actually Item Search!!!!!!!
+    {
+        private Models.DbModel db = new Models.DbModel();
+        public IHttpActionResult GetCustName(string searcher)
+        {
+
+            Models.DbModel db = new Models.DbModel();
+            var result = db.Items.Where(x => x.Name.StartsWith(searcher) || searcher == null).ToList();
+            return Ok(result);
+    }
+
+
+    }
+}
+
