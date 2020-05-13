284efd9f1347d2b2bdded2826c4746fee6bed1a1
DanteXYZ <47111008+DanteXYZ@users.noreply.github.com>
2020-05-13 09:50:22 -0500

    API Model,View & Controller

---
+++ build/API Component/Controllers/ItemController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Xml.Linq;
+using DefCanApi.Models;
+using Newtonsoft.Json;
+
+namespace DefCanApi.Controllers
+{
+    //[RoutePrefix("api/Item")]
+    public class ItemController : ApiController
+    {
+        private Models.DbModel db = new Models.DbModel();
+
+        // GET: api/Item
+        public IQueryable<Item> GetItems()
+        {
+            return db.Items;
+        }
+
+        // GET: api/Item/5
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult GetItem(int id)
+        {
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        // PUT: api/Item/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutItem(int id, Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != item.ItemID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Item
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult PostItem(Item item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Items.Add(item);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = item.ItemID }, item);
+        }
+
+        // DELETE: api/Item/5
+        [ResponseType(typeof(Item))]
+        public IHttpActionResult DeleteItem(int id)
+        {
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            db.Items.Remove(item);
+            db.SaveChanges();
+
+            return Ok(item);
+        }
+
+
+        [HttpGet]
+        [Route("Search/{Name}")]
+        public HttpResponseMessage Search(string Name)
+        {
+            try
+            {
+                var httpResponseMessage = new HttpResponseMessage();
+                httpResponseMessage.Content = new StringContent
+                    (JsonConvert.SerializeObject(db.Items.Where(p => p.Name.Contains(Name)).ToList()));
+                httpResponseMessage.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                return httpResponseMessage;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ItemExists(int id)
+        {
+            return db.Items.Count(e => e.ItemID == id) > 0;
+        }
+    }
+}
